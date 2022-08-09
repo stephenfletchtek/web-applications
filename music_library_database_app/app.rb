1222af35ee6d910aea5480053facd828896699fb
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-09 11:09:24 +0100

    must_library POST /albums implemented and tested

--- music_library_database_app/app.rb
+++ music_library_database_app/app.rb
@@ -13,4 +13,14 @@ class Application < Sinatra::Base
     also_reload 'lib/album_repository'
     also_reload 'lib/artist_repository'
   end
+
+  post '/albums' do
+    new_album = Album.new
+    new_album.title = params[:title]
+    new_album.release_year = params[:release_year]
+    new_album.artist_id = params[:artist_id]
+    repo = AlbumRepository.new
+    repo.create(new_album)
+  end
+
 end
--- music_library_database_app/spec/integration/application_spec.rb
+++ music_library_database_app/spec/integration/application_spec.rb
@@ -10,4 +10,15 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context "POST /albums" do
+    it 'returns 200 OK' do
+      response = post('/albums', title: 'Voyage', release_year: '2022', artist_id: '2')
+      expect(response.status).to eq(200)
+      repo = AlbumRepository.new
+      latest_album = repo.all[-1]
+      expect(latest_album.title).to eq('Voyage')
+      expect(latest_album.release_year).to eq('2022')
+      expect(latest_album.artist_id).to eq(2)
+    end
+  end
 end
